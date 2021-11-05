f01e09bd7d24f1f588d575ce0b2321dc3819bf78
sangki2070 <devsangki@gmail.com>
2021-11-06 03:39:04 +0900

    SK21 List header v1

--- sleepy_mobile/src/conponents/list/List.styles.ts
+++ sleepy_mobile/src/conponents/list/List.styles.ts
@@ -1,10 +1,6 @@
 import styled from "@emotion/native";
 
-export const ListView = styled.View`
-  flex: 1;
-  align-items: center;
-  background-color: #e24e4a;
-`;
+export const ListView = styled.View``;
 
 export const ListText = styled.Text`
   font-size: 50px;
