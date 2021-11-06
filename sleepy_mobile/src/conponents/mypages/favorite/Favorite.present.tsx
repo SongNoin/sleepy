0b292b8eece5d91ec24799b281ca802e1f606255
SongNoin <as5427072@gmail.com>
2021-11-06 16:21:34 +0900

    KH25 Complete mypage css

--- sleepy_mobile/src/conponents/mypages/favorite/Favorite.present.tsx
+++ sleepy_mobile/src/conponents/mypages/favorite/Favorite.present.tsx
@@ -1,12 +1,42 @@
 import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
 
-import { FavoriteView, FavoriteText } from "./Favorite.styles";
+import {
+  FavoriteView,
+  FavoriteCountWrapper,
+  FavoriteCount,
+  FavoriteCountContent,
+  FavoriteCountUnit,
+  FavoriteListWrapper,
+  FavoriteCard,
+  FavoriteImage,
+  FavoriteContent,
+  FavoriteName,
+  FavoritePrice,
+} from "./Favorite.styles";
 
 const FavoriteUI = () => {
   return (
-    <FavoriteView>
-      <FavoriteText>마이찜 페이지입니다.</FavoriteText>
-    </FavoriteView>
+    <ScrollView>
+      <FavoriteView>
+        <FavoriteCountWrapper>
+          <FavoriteCount>찜 한 갯수 :</FavoriteCount>
+          <FavoriteCountContent>1000</FavoriteCountContent>
+          <FavoriteCountUnit>개</FavoriteCountUnit>
+        </FavoriteCountWrapper>
+        <FavoriteListWrapper>
+          <FavoriteCard>
+            <FavoriteImage
+              source={require("../../../../public/images/mypage/buyhistoryproductphoto.png")}
+            />
+            <FavoriteContent>
+              <FavoriteName>글자는띄어쓰기없이</FavoriteName>
+              <FavoritePrice>199000원</FavoritePrice>
+            </FavoriteContent>
+          </FavoriteCard>
+        </FavoriteListWrapper>
+      </FavoriteView>
+    </ScrollView>
   );
 };
 
