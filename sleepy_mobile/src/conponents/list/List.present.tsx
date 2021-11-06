5a83689914a0612a17e4e2f1b63a607aeda41e6a
sangki2070 <devsangki@gmail.com>
2021-11-06 16:21:38 +0900

    SK21 List screen css complete

--- sleepy_mobile/src/conponents/list/List.present.tsx
+++ sleepy_mobile/src/conponents/list/List.present.tsx
@@ -1,11 +1,87 @@
 import React from "react";
 
-import { ListView, ListText } from "./List.styles";
+import {
+  ListView,
+  ProductImageWrapper,
+  ProductImage,
+  DetailProductWrapper,
+  InfoWrapper,
+  InfoTextWrapper,
+  InfoFavoriteImage,
+  InfoTitle,
+  InfoPrice,
+} from "./List.styles";
+
+import HeaderAnimation from "./List.header.animation";
+
+import { ScrollView } from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
 
 const ListUI = () => {
+  const navigation = useNavigation();
+
   return (
     <ListView>
-      <ListText>여기는 리스트 페이지입니다.</ListText>
+      <HeaderAnimation />
+      <ScrollView>
+        <DetailProductWrapper>
+          <ProductImageWrapper
+            onPress={() => navigation.navigate("상품 상세보기")}
+          >
+            <ProductImage
+              source={require("../../../public/images/list/productimage.png")}
+            />
+          </ProductImageWrapper>
+          <InfoWrapper>
+            <InfoTextWrapper>
+              <InfoTitle>
+                글자는 띄어쓰기 포함 이정도로 맞추면 될 것 같아요.
+              </InfoTitle>
+              <InfoPrice>1999999999999999999999원</InfoPrice>
+            </InfoTextWrapper>
+            <InfoFavoriteImage
+              source={require("../../../public/images/list/infofavorite.png")}
+            />
+          </InfoWrapper>
+        </DetailProductWrapper>
+        <DetailProductWrapper>
+          <ProductImageWrapper>
+            <ProductImage
+              source={require("../../../public/images/list/productimage.png")}
+            />
+          </ProductImageWrapper>
+          <InfoWrapper>
+            <InfoTextWrapper>
+              <InfoTitle>
+                글자는 띄어쓰기 포함 이정도로 맞추면 될 것 같아요.
+              </InfoTitle>
+              <InfoPrice>1999999999999999999999원</InfoPrice>
+            </InfoTextWrapper>
+            <InfoFavoriteImage
+              source={require("../../../public/images/list/infofavorite.png")}
+            />
+          </InfoWrapper>
+        </DetailProductWrapper>
+        <DetailProductWrapper>
+          <ProductImageWrapper>
+            <ProductImage
+              source={require("../../../public/images/list/productimage.png")}
+            />
+          </ProductImageWrapper>
+          <InfoWrapper>
+            <InfoTextWrapper>
+              <InfoTitle>
+                글자는 띄어쓰기 포함 이정도로 맞추면 될 것 같아요.
+              </InfoTitle>
+              <InfoPrice>1999999999999999999999원</InfoPrice>
+            </InfoTextWrapper>
+            <InfoFavoriteImage
+              source={require("../../../public/images/list/infofavorite.png")}
+            />
+          </InfoWrapper>
+        </DetailProductWrapper>
+      </ScrollView>
     </ListView>
   );
 };
