e542eba184ba59d2778c6aad6f349cc2177eb122
hyeseonhan <h1225hs@gmail.com>
2021-11-12 16:48:45 +0900

    Web/ HS87 Make web overall admin page and tables

--- sleepy_web_admin/src/components/units/productstable/productstable.container.tsx
+++ sleepy_web_admin/src/components/units/productstable/productstable.container.tsx
@@ -1,5 +1,11 @@
+import { useQuery } from "@apollo/client";
 import ProductstableUI from "./productstable.presenter";
+import { FETCH_USED_ITEMS_I_SOLD } from "./productstable.queries";
 
 export default function Productstable() {
-  return <ProductstableUI />;
+  const { data } = useQuery(FETCH_USED_ITEMS_I_SOLD, {
+    variables: { page: 1 },
+  });
+
+  return <ProductstableUI data={data} />;
 }
