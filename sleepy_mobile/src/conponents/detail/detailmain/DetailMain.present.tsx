5f4898b3a4e2b018b8d789e2c9588aeb2961c310
hyeseonhan <h1225hs@gmail.com>
2021-11-17 00:33:41 +0900

    HS139 EFix review reload

--- sleepy_mobile/src/conponents/detail/detailmain/DetailMain.present.tsx
+++ sleepy_mobile/src/conponents/detail/detailmain/DetailMain.present.tsx
@@ -23,61 +23,65 @@ import RenderHTML from "react-native-render-html";
 const DetailUI = (props: any) => {
   return (
     <ScrollView>
-      <Wrapper>
-        <ProductWrapper>
-          <ProductImages data={props.data} />
-          <ReviewInfoWrapper>
-            <ReviewStar
-              source={require("../../../../public/images/detail/star.png")}
-            />
-            <ReviewStar
-              source={require("../../../../public/images/detail/star.png")}
-            />
-            <ReviewStar
-              source={require("../../../../public/images/detail/star.png")}
-            />
-            <ReviewStar
-              source={require("../../../../public/images/detail/starempty.png")}
-            />
-            <ReviewStar
-              source={require("../../../../public/images/detail/starempty.png")}
-            />
-            <ReviewAverage>(3.25)</ReviewAverage>
-          </ReviewInfoWrapper>
-          <ProductName>{props.data?.fetchUseditem.name}</ProductName>
-          <ProductPrice>
-            {props.data?.fetchUseditem.price
-              .toLocaleString("ko-KR")
-              .toString()
-              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
-            원
-          </ProductPrice>
-        </ProductWrapper>
-        <DetaillWrapper>
-          <DetailContent>
-            <RenderHTML
-              source={{ html: `${props.data?.fetchUseditem.contents}` }}
-            />
-          </DetailContent>
-          {props.data?.fetchUseditem.images
-            ?.filter((el) => el)
-            .map((el) => (
-              <Photo
-                key={el}
-                source={{ uri: `https://storage.googleapis.com/${el}` }}
-              />
-            ))}
-          {/* <Photo
+      {props.isReview && (
+        <>
+          <Wrapper>
+            <ProductWrapper>
+              <ProductImages data={props.data} />
+              <ReviewInfoWrapper>
+                <ReviewStar
+                  source={require("../../../../public/images/detail/star.png")}
+                />
+                <ReviewStar
+                  source={require("../../../../public/images/detail/star.png")}
+                />
+                <ReviewStar
+                  source={require("../../../../public/images/detail/star.png")}
+                />
+                <ReviewStar
+                  source={require("../../../../public/images/detail/starempty.png")}
+                />
+                <ReviewStar
+                  source={require("../../../../public/images/detail/starempty.png")}
+                />
+                <ReviewAverage>(3.25)</ReviewAverage>
+              </ReviewInfoWrapper>
+              <ProductName>{props.data?.fetchUseditem.name}</ProductName>
+              <ProductPrice>
+                {props.data?.fetchUseditem.price
+                  .toLocaleString("ko-KR")
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+                원
+              </ProductPrice>
+            </ProductWrapper>
+            <DetaillWrapper>
+              <DetailContent>
+                <RenderHTML
+                  source={{ html: `${props.data?.fetchUseditem.contents}` }}
+                />
+              </DetailContent>
+              {props.data?.fetchUseditem.images
+                ?.filter((el) => el)
+                .map((el) => (
+                  <Photo
+                    key={el}
+                    source={{ uri: `https://storage.googleapis.com/${el}` }}
+                  />
+                ))}
+              {/* <Photo
             source={{
               uri: `https://storage.googleapis.com/${props.data?.fetchUseditem.images[0]}`,
             }}
           /> */}
-        </DetaillWrapper>
-      </Wrapper>
-      <ReviewWrapper>
-        <Title>리뷰</Title>
-        <ReviewListContainer data={props.data} />
-      </ReviewWrapper>
+            </DetaillWrapper>
+          </Wrapper>
+          <ReviewWrapper>
+            <Title>리뷰</Title>
+            <ReviewListContainer data={props.data} />
+          </ReviewWrapper>
+        </>
+      )}
     </ScrollView>
   );
 };
