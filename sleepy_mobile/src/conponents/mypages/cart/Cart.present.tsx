01fe7a5341980b2614428a3a42a444e70005899b
BYEONGJIN-AHN <byungjin0120@gmail.com>
2021-11-18 16:02:04 +0900

    BJ153 Complete CartPage

--- sleepy_mobile/src/conponents/mypages/cart/Cart.present.tsx
+++ sleepy_mobile/src/conponents/mypages/cart/Cart.present.tsx
@@ -3,71 +3,73 @@ import { ScrollView } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
 import {
   MyCartView,
-  CartCountWrapper,
-  CartCount,
-  CartCountContent,
   CartListWrapper,
   CartCard,
   CartImage,
   CartContent,
   CartName,
   CartPrice,
-  TotalPriceWrapper,
-  CartCardWrapper,
+  CartImageWrapper,
+  DelButton,
+  BuyButton,
+  BuyButtonText,
 } from "./Cart.styles";
+import { Text } from "react-native";
+import DeleteIcon from "react-native-vector-icons/AntDesign";
 
 const CartUI = (props: any) => {
   const navigation = useNavigation();
+  const soldOutList = props.soldOutList?.map((el: any) => el._id);
+
   return (
-    <ScrollView>
+    <ScrollView style={{ backgroundColor: "white" }}>
       <MyCartView>
-        <CartCountWrapper>
-          <CartCount>상품가격</CartCount>
-          <CartCountContent>{`${
-            !props.getPrices ? 0 : props.getPrices
-          }원`}</CartCountContent>
-        </CartCountWrapper>
         <CartListWrapper>
+          {!props.productInfo && <Text>장바구니가 비어있습니다!</Text>}
+
           {props.productInfo
             ?.map((el: any) => (
-              <CartCardWrapper
-                key={el.id}
-                onPress={() => {
-                  navigation.navigate("상품 상세보기", {
-                    id: props.onPressDetail(el),
-                  });
-                }}
-              >
-                <CartCard>
+              <CartCard isSoldOut={soldOutList?.includes(el.id)} key={el.id}>
+                <CartImageWrapper
+                  onPress={() => {
+                    if (!soldOutList?.includes(el.id)) {
+                      navigation.navigate("상품 상세보기", {
+                        id: props.onPressDetail(el),
+                      });
+                    }
+                  }}
+                >
                   <CartImage
                     source={{
                       uri: `https://storage.googleapis.com/${el.images[0]}`,
                     }}
                   />
-                  <CartContent>
-                    <CartName>
-                      {el.productName.length > 9
-                        ? `${el.productName.substr(0, 10)}..`
-                        : el.productName}
-                    </CartName>
-                    <CartPrice>{`${el.productPrice}원`}</CartPrice>
-                  </CartContent>
-                </CartCard>
-              </CartCardWrapper>
+                </CartImageWrapper>
+                <CartContent>
+                  <CartName>
+                    {el.productName.length > 9
+                      ? `${el.productName.substr(0, 10)}..`
+                      : el.productName}
+                  </CartName>
+
+                  <CartPrice>{`${el.productPrice}원`}</CartPrice>
+
+                  {!soldOutList?.includes(el.id) && (
+                    <BuyButton onPress={props.buyMyFavoritePr(el)}>
+                      <BuyButtonText>구매하기</BuyButtonText>
+                    </BuyButton>
+                  )}
+
+                  <DelButton onPress={props.deleteMyFavoritePr(el)}>
+                    <Text>
+                      <DeleteIcon name="close" size={20} color="black" />
+                    </Text>
+                  </DelButton>
+                </CartContent>
+              </CartCard>
             ))
             .reverse()}
         </CartListWrapper>
-        <CartCountWrapper></CartCountWrapper>
-        <TotalPriceWrapper>
-          <CartCount>배송비</CartCount>
-          <CartCountContent>+3000원</CartCountContent>
-        </TotalPriceWrapper>
-        <TotalPriceWrapper>
-          <CartCount>총 합산 가격</CartCount>
-          <CartCountContent>{`${
-            !props.getPrices ? 0 : props.getPrices + 3000
-          }원`}</CartCountContent>
-        </TotalPriceWrapper>
       </MyCartView>
     </ScrollView>
   );
--- sleepy_mobile/src/conponents/mypages/cart/Cart.styles.ts
+++ sleepy_mobile/src/conponents/mypages/cart/Cart.styles.ts
@@ -1,105 +1,93 @@
 import styled from "@emotion/native";
 
+interface IProps {
+  isSoldOut: Boolean;
+}
+
 export const MyCartView = styled.View`
   flex: 1;
   background-color: white;
   padding-bottom: 20px;
 `;
 
-export const CartCountWrapper = styled.View`
-  display: flex;
-  flex-direction: row;
-
-  justify-content: space-between;
-  align-items: center;
-
-  margin-left: 18px;
-  margin-right: 18px;
-  margin-top: 24px;
-
-  padding-bottom: 12px;
-  padding-left: 9px;
-  padding-right: 9px;
-  border-bottom-width: 2px;
-  border-bottom-color: #f1eef6;
-`;
-export const CartCount = styled.Text`
-  font-family: "NotoSans-Bold";
-  font-style: normal;
-  font-weight: 600;
-  font-size: 18px;
-  line-height: 25px;
-  color: #3b3b3b;
-`;
-export const CartCountContent = styled.Text`
-  font-family: "NotoSans-Bold";
-  font-style: normal;
-  font-weight: 600;
-  font-size: 18px;
-  line-height: 25px;
-
-  color: #5b5bc0;
-`;
-
 export const CartListWrapper = styled.View`
   display: flex;
   align-items: center;
 `;
+
 export const CartCard = styled.View`
   width: 339px;
   height: 140px;
-
+  opacity: ${(props: IProps) => (props.isSoldOut ? "0.4" : "1")};
   border: 1px solid #e2e2ff;
   box-shadow: 0px 4px 8px black;
   border-radius: 15px;
   display: flex;
   flex-direction: row;
   align-items: center;
-
   margin-top: 12px;
   padding-left: 25px;
 `;
+
 export const CartImage = styled.Image`
   width: 110px;
   height: 105px;
   border-radius: 15px;
 `;
+
 export const CartContent = styled.View`
   margin-left: 15px;
   display: flex;
+  padding-bottom: 35px;
 `;
+
 export const CartName = styled.Text`
   font-family: "NotoSans-Bold";
   font-style: normal;
   font-weight: 500;
   font-size: 16px;
   line-height: 22px;
-
   color: #3c3c3c;
-  margin-bottom: 20px;
+  margin-bottom: 5px;
 `;
+
 export const CartPrice = styled.Text`
   font-family: "NotoSans-Bold";
   font-style: normal;
   font-weight: 600;
   font-size: 14px;
   line-height: 19px;
-
   color: #5b5bc0;
 `;
 
-export const TotalPriceWrapper = styled.View`
-  display: flex;
-  flex-direction: row;
-
-  justify-content: space-between;
-  align-items: center;
-
-  margin-top: 12px;
-  margin-left: 18px;
-  margin-right: 18px;
-  padding-left: 9px;
-  padding-right: 9px;
+export const DelButton = styled.TouchableOpacity`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  left: 152px;
+  bottom: 70px;
 `;
 
-export const CartCardWrapper = styled.TouchableOpacity``;
+export const BuyButton = styled.TouchableOpacity`
+  height: 20px;
+  position: absolute;
+  top: 60px;
+  border: none;
+  background-color: #5b5bc0;
+  height: 25px;
+  width: 65px;
+  display: flex;
+  justify-content: center;
+  border-radius: 5px;
+  align-items: center;
+`;
+
+export const BuyButtonText = styled.Text`
+  font-family: "NotoSans-Bold";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 10px;
+  color: white;
+`;
+
+export const CartImageWrapper = styled.TouchableOpacity``;
